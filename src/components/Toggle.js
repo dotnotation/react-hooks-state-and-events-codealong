00586ec6f83d820245c1813296bf38ab580064bb
dotnotation <dotinmotionproductions@gmail.com>
2022-04-19 21:21:44 -0400

    finished codealong

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,6 +2,7 @@ import {useState} from "react";
 
 function Toggle() {
   const [toggle, setToggle] = useState("OFF")
+  const [isOn, setIsOn] = useState(false);
 
   function switchToggle(){
     if (toggle === "OFF") {
@@ -10,7 +11,20 @@ function Toggle() {
       setToggle("OFF")
     }
   }
-  return <button onClick={switchToggle}>{toggle}</button>;
-}
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <div>
+      <button onClick={switchToggle}>{toggle}</button> 
+      <button style={{ background: color }} onClick={handleClick}>
+        {isOn ? "ON" : "OFF"}
+      </button> 
+    </div>
+  )}
 
 export default Toggle;
